c356ed32bb16be35178d7519298d353b05b8ae8c
Tim Porritt <cimera2@gmail.com>
2018-07-25 20:03:06 +1000

    Forgot to remove statics testing

--- src/main.cpp
+++ src/main.cpp
@@ -13,46 +13,9 @@ void windowCloseEvent(GLFWwindow *closingWindow)
 	shouldExit = true;
 }
 
-struct Holder
-{
-	virtual std::string getname() = 0;
-};
-
-template <typename T>
-struct Base: public Holder
-{
-	static const std::string name;
-	std::string getname() { return name; }
-};
-
-struct A : public Base<A>
-{
-	A(int innum) : num(innum) {
-	}
-	int num;
-};
-template<> const std::string Base<A>::name = "a";
-
-struct B : public Base<B>
-{
-};
-template<> const std::string Base<B>::name = "b";
-
-std::vector<Holder*> vec;
-
 int main()
 {
 	Logger() << "First Line of Program";
-
-	vec.emplace_back(new A(24));
-	vec.emplace_back(new B());
-
-	for(auto d : vec)
-		Logger() << d->getname();
-
-	auto a = dynamic_cast<A*>(vec[0]);
-	Logger() << a->num;
-
 
 	initGLFW();
 
